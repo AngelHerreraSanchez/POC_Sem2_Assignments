61b2fabfaf4d3dc9a796de1adbafb8a5e445bb1b
Angel Herrera-Sanchez <herreang000@warren.k12.in.us>
2024-01-12 11:42:35 -0500

    updates

--- 01_Strings/02_Basic String Operations/Assignment.py
+++ 01_Strings/02_Basic String Operations/Assignment.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 bit_of_text = input("Enter some text: ")#YOUDO get this from user input.  
 
 for char in bit_of_text:
     print(ord(char))#YOUDO:  print char on its own line.  You will need ord to do this.  
-=======
-bit_of_text = "" #YOUDO get this from user input.  
-
-for char in bit_of_text:
-    #YOUDO:  print char on its own line.  You will need ord to do this.  
-    pass #YOUDO:  remove the pass when done.  
->>>>>>> 18953c5a75fd7be97d690136a14483d63fae963a
