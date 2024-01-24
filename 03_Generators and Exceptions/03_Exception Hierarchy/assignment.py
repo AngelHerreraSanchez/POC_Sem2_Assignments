9fccfa62ee9d5c4694979278a8566c02c83b893e
Angel Herrera-Sanchez <herreang000@warren.k12.in.us>
2024-01-24 11:51:30 -0500

    Assignment3.3

---
+++ 03_Generators and Exceptions/03_Exception Hierarchy/assignment.py
@@ -0,0 +1,10 @@
+try:
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter another number"))
+except ValueError:
+    print("An interger wasn't given")
+
+try:
+    print(number1/number2)
+except ZeroDivisionError:
+    print("Division by zero is not possible") 
