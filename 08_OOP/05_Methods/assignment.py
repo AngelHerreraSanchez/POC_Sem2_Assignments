24ea7c7c7e19317cecee54c9bb285f1f12784bce
Angel Herrera-Sanchez <herreang000@warren.k12.in.us>
2024-03-08 12:01:58 -0500

    Assignment8.5

--- 08_OOP/05_Methods/assignment.py
+++ 08_OOP/05_Methods/assignment.py
@@ -1,31 +1,50 @@
 class Rectangle:
+    __counter = 0 
+
+
+    def get_count():
+        return Rectangle.__counter
+    
+
+
 
     def __init__(self, base: float, height: float) -> None:
-        if (base < 0):
+        if(base < 0):
             self.__base = 0
-        else:
+        else:       
             self.__base = base
-
-        if (height < 0):
+            
+        if(height < 0):
             self.__height = 0
         else:
             self.__height = height
-
+        
     def get_height(self) -> float:
         return self.__height
+    
+    def get_base(self) -> float:
+        return self.__base 
 
-    # YOUDO the get_base method
-
+    
     def get_perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
-
-    # Youdo get_area method
+    
+    def get_area(self) -> float:
+        return self.__height * self.__base
     
     def __str__(self) -> str:
-        # Rectangle of base:3, height:4
-        return "Rectangle of base:" + self.__base + ", height:" + self.__height
-        pass
+        return "Rectangle with base:" + str(self.__base) + ", height:" + str(self.__height)
+
+    
+ 
+ 
+ 
+Rectangle_1 = Rectangle(5, 8)
+print(Rectangle_1)
 
 
-# YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-# using only the methods not the fields/property/attributes
+Rectangle_2 = Rectangle(7, 10)
+print(Rectangle_2)
+
+
+print(Rectangle.get_count())
