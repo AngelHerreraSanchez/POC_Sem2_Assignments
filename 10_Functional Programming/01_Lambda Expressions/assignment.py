1d75ceff67d6bed3096010f343827e8f10c8ad51
Angel Herrera-Sanchez <herreang000@warren.k12.in.us>
2024-03-19 11:55:04 -0400

    Assignment10.1

---
+++ 10_Functional Programming/01_Lambda Expressions/assignment.py
@@ -0,0 +1,21 @@
+def product(x, y):
+    return x * y
+
+lambda x, y: x * y
+
+print(product(15, 13))
+
+def quotient(x, y):
+    return x / y
+
+lambda x, y: x / y
+
+print(quotient(15, 13))
+
+def exponent(x, y):
+    return x ** y
+
+lambda x, y: x ** y
+
+print(exponent(3, 7))
+
